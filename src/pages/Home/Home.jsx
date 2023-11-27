c4eb7599f76a61b7b4a83151f9ae3be24752463a
Tasin007 <mdtasinalam.mta@gmail.com>
2023-11-27 20:56:06 +0600

    json created and banner and navbar edited

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
+import Banner from "./Banner";
 
 
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             
-            <h2>this is home</h2>
         </div>
     );
 };
